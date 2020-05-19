9b7e16a61a602a1265892b97a0e6d10d3bc442ba
Isidora Valenzuela <isi.valenzuela@gmail.com>
2020-05-19 10:46:08 -0400

    dos desafíos mas

---
+++ cachipun.rb
@@ -0,0 +1,72 @@
+jugador = ARGV[0].downcase
+
+computador = Random.rand(0..2)
+# # 0 = piedra ; 1=papel ; 2=tijera
+# if jugador != "piedra" && jugador != "papel" && jugador != "tijera" 
+#     puts"argumento invalido: debe ser piedra, papel o tijera"
+# else
+#     if jugador == "piedra" 
+
+#         if computador==0
+#             puts "computador juega piedra\n empate"
+
+#         elsif computador==1
+#             puts "computador juega papel\ncomputador gana"
+
+#         elsif computador==2
+#             puts "computador juega tijera\njugador gana"
+
+#         end
+#     end
+
+#     if jugador == "papel"
+#         if computador==0
+#             puts "computador juega piedra\n jugador gana"
+#         elsif computador==1
+#             puts "computador juega papel\n empate"
+#         else
+#             puts "computador juega tijera\ncomputador gana"
+#         end
+#     end
+
+#     if jugador == "tijera"
+#         if computador==0
+#             puts "computador juega piedra\ncomputador gana"
+#         elsif computador==1
+#             puts "computador juega papel\n jugador gana"
+#         else
+#             puts "computador juega tijera\nempate"
+#         end
+#     end
+# end
+case (jugador)
+when "piedra"
+    if computador==0
+        puts "computador juega piedra\n empate"
+        
+    elsif computador==1
+        puts "computador juega papel\ncomputador gana"
+        
+    elsif computador==2
+        puts "computador juega tijera\njugador gana"
+        
+    end
+when "papel"
+    if computador==0
+        puts "computador juega piedra\n jugador gana"
+    elsif computador==1
+        puts "computador juega papel\n empate"
+    else
+        puts "computador juega tijera\ncomputador gana"
+    end
+when "tijera"
+    if computador==0
+        puts "computador juega piedra\ncomputador gana"
+    elsif computador==1
+        puts "computador juega papel\n jugador gana"
+    else
+        puts "computador juega tijera\nempate"
+    end
+else
+    puts"argumento invalido: debe ser piedra, papel o tijera"
+end
